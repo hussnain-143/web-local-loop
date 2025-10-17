5fb357b4ee19321335cb1f6eea8cbdc94ff7f317
Hussnain Ahmed <143833798+hussnain-143@users.noreply.github.com>
2025-10-17 16:14:56 +0500

    set the emails templates and set adv

--- src/app/components/AdvertisePage.tsx
+++ src/app/components/AdvertisePage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import React, { useState, useTransition, useRef } from "react";
 import { FiDollarSign, FiMail, FiUsers, FiSend, FiPhone } from "react-icons/fi";
